579dbfe5c3a8ee398a63201a30675b8a4e927950
alexmeh96 <alex.meh96@mail.ru>
2021-01-04 02:00:55 +0300

    add getUser

--- src/main/java/com/example/backendapp/MainControl.java
+++ src/main/java/com/example/backendapp/MainControl.java
@@ -3,6 +3,7 @@ package com.example.backendapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +15,13 @@ public class MainControl {
     private UserRepo userRepo;
 
     @GetMapping("/users")
-    public List<User> main() {
+    public List<User> allUsers() {
 
         return userRepo.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable(name = "id") User user) {
+        return user;
     }
 }
